3f6e21ddc0c2654e5e3258e30bb4bb6332a3bdda
StellaContrail <sky.contrail.man+github@gmail.com>
2017-09-20 01:29:35 +0900

    [追加] 火炎攻撃、火の玉攻撃のスキルの骨組みを追加

---
+++ Assets/Scripts/Player/Skills/FireballMagic/FireballMagic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballMagic : MonoBehaviour {
+
+    // Which is faster in calculating?
+    // Moving with Physics2D.velocity
+    // Moving with DOTween.MoveTo
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
