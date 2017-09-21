7f06d9a7c2b9b10664a7d96694b518d861607f68
StellaContrail <sky.contrail.man+github@gmail.com>
2017-09-22 03:16:28 +0900

    [追加] Playerの移動関係のスクリプトを実装
    * ActionManagerに対応したPlayerのキー操作による移動スクリプトを追加
    * 完全にIdle状態になった時にIsDone() = trueを返し、isSatisfied = falseとなる

---
+++ Assets/Scripts/Player/Walk/PlayerWalkCondition.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalkCondition : Condition {
+
+    public enum WALK_DIR
+    {
+        IDLE, RIGHT, LEFT
+    }
+
+    WALK_DIR movingDirection = WALK_DIR.IDLE;
+
+    ConditionState Status;
+	public override ConditionState Check()
+	{
+        return Status;
+    }
+    
+	// Use this for initialization
+    void Start()
+    {
+        Status = new ConditionState();
+        Status.args.Add("movingDirection", WALK_DIR.IDLE);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            movingDirection = WALK_DIR.LEFT;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            movingDirection = WALK_DIR.RIGHT;
+        }
+        else
+        {
+            movingDirection = WALK_DIR.IDLE;
+        }
+        // movingDirectionをstring型でPlayerWalk.csに渡してPlayerWalk.csでenum型に直す
+        Status.args["movingDirection"] = movingDirection.ToString();
+
+        // Idle => Idle時の移行では常にisSatisfiedはfalseになる.
+        // それ以外の場合はActionを常に実行しなければいけないのでtrue
+        Status.isSatisfied = gameObject.GetComponent<Rigidbody2D>().velocity.x != 0f || movingDirection != WALK_DIR.IDLE;
+    }
+}
